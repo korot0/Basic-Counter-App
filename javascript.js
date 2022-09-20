90f68eb1f637dc8c97ba2975bae1c91e3cf9359e
korot0 <108902180+korot0@users.noreply.github.com>
2022-09-19 21:01:27 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count++
+    countEl.textContent = count
+}
+
+function decrement() {
+    count--
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " , "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
